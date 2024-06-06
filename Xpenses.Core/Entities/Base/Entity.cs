a961cbe4c63e62be71aef9770eb4009ccec82423
Vinicius <vini383@gmail.com>
2024-06-06 09:55:12 -0300

    feat: init solution

---
+++ Xpenses.Core/Entities/Base/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace Xpenses.Core.Entities.Base;
+
+public class Entity
+{
+    public long Id { get; set; }
+
+}
---
+++ Xpenses.Core/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿
+using Xpenses.Core.Entities.Base;
+
+namespace Xpenses.Core.Entities;
+
+public class Category : Entity
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+    
+}
