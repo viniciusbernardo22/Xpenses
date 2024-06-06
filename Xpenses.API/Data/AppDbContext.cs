33b7de662fd64745c32a719d7ab6d19ff2a085ae
Vinicius <vini383@gmail.com>
2024-06-06 15:17:22 -0300

    Add data context and mappings

---
+++ Xpenses.API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Xpenses.Core.Entities;
+
+namespace Xpenses.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    
+}
---
+++ Xpenses.API/Data/Mappings/TransactionMapping.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Xpenses.Core.Entities;
+
+namespace Xpenses.API.Data.Mappings;
+
+public class TransactionMapping : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> b)
+    {
+        b.ToTable("Transaction");
+        b.HasKey(x => x.Id);
+        
+        b.Property(x => x.Title)
+            .IsRequired(true)
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(80);
+
+        b.Property(x => x.Type)
+            .IsRequired(true)
+            .HasColumnType("SMALLINT");
+
+        b.Property(x => x.Amount)
+            .IsRequired(true)
+            .HasColumnType("MONEY")
+            .HasMaxLength(255);
+        
+        
+        b.Property(x => x.CreatedAt)
+            .IsRequired(true);
+
+        b.Property(x => x.PaidOrReceivedAt)
+            .IsRequired(false);
+        
+        b.Property(x => x.UserId)
+            .IsRequired(true)
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(80);
+
+    }
+}
