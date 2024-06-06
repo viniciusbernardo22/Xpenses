c1cff8fddd00a76e0ebe3920184f93200e990fa6
Vinicius <vini383@gmail.com>
2024-06-06 11:11:08 -0300

    Add packages and API handlers

--- Xpenses.API/Program.cs
+++ Xpenses.API/Program.cs
@@ -1,6 +1,41 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+string version = "v1";
+
+app.MapPost($"/{version}/transaction", (Request request, Handler handler) => handler.Handle(request))
+    .WithName("Transactions: Create")
+    .WithSummary("Cria uma nova transação")
+    .Produces<Response>();
 
 app.Run();
+
+public class Request
+{
+    public string Title { get; set; } = String.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public int Type { get; set; }
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    
+}
+
+public class Response
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = String.Empty;
+}
+
+
+public class Handler
+{
+    public Response Handle(Request request)
+    {
+        return new Response
+        {
+            Id = 4,
+            Title = request.Title
+        };
+    } 
+}
