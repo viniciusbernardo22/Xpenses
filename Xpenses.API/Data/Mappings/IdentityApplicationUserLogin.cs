8b7390a18ca4d61c14eb0fd93f8cedf44fed2a2d
Vinicius <vini383@gmail.com>
2024-06-19 15:52:10 -0300

    feat> add IdentityApplicationUserLogin.cs Mapping

---
+++ Xpenses.API/Data/Mappings/IdentityApplicationUserLogin.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Xpenses.API.Data.Mappings;
+
+public class IdentityApplicationUserLogin : IEntityTypeConfiguration<IdentityUserLogin<long>>
+{
+    public void Configure(EntityTypeBuilder<IdentityUserLogin<long>> b)
+    {
+        b.ToTable("IdentityUserLogin");
+        b.HasKey(l => new
+        {
+            l.LoginProvider, l.ProviderKey
+        });
+        b.Property(l => l.LoginProvider).HasMaxLength(128);
+        b.Property(l => l.ProviderKey).HasMaxLength(128);
+        b.Property(l => l.ProviderDisplayName).HasMaxLength(255);
+    }
+}
