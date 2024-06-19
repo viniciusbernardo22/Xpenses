8734db179f6cffec6d6635335c31aacd37f37195
Vinicius <vini383@gmail.com>
2024-06-19 11:11:32 -0300

    add GetTransactionByIdEndpoint.cs

---
+++ Xpenses.API/Endpoints/Transactions/GetTransactionByIdEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using Xpenses.API.Common.Api;
+using Xpenses.Core.Entities;
+using Xpenses.Core.Handlers;
+using Xpenses.Core.Requests.Transactions;
+using Xpenses.Core.Responses;
+
+namespace Xpenses.API.Endpoints.Transactions;
+
+public class GetTransactionByIdEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app) 
+        => app.MapGet("/{id}", HandleAsync)
+        .WithName("Transactions: Get By Id")
+        .WithSummary("Busca uma transação pelo Id")
+        .Produces<Response<Transaction?>>();
+
+    private static async Task<IResult> HandleAsync(ITransactionHandler handler, long id)
+    {
+        var request = new GetTransactionByIdRequest
+        {
+            Id = id,
+            UserId = "123"
+        };
+
+        var response = await handler.GetByIdAsync(request);
+
+        return response.IsSuccess
+            ? TypedResults.Ok(response)
+            : TypedResults.BadRequest(response.Data);
+    }
+}
