03a532d8eb0d491a10cff34042ba48262aa00385
Vinicius <vini383@gmail.com>
2024-06-19 10:23:32 -0300

    add CreateTransactionEndpoint.cs

---
+++ Xpenses.API/Endpoints/Transactions/CreateTransactionEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Xpenses.API.Common.Api;
+using Xpenses.Core.Entities;
+using Xpenses.Core.Handlers;
+using Xpenses.Core.Requests.Transactions;
+using Xpenses.Core.Responses;
+
+namespace Xpenses.API.Endpoints.Transactions;
+
+public class CreateTransactionEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapPost("/", HandleAsync)
+            .WithName("Transactions: Create")
+            .WithSummary("Cria uma nova transação")
+            .Produces<Response<Transaction?>>();
+
+
+    private static async Task<IResult> HandleAsync(ITransactionHandler handler, CreateTransactionRequest request)
+    {
+        var userId = "123";
+        var result = await handler.CreateAsync(request);
+        return result.IsSuccess
+            ? TypedResults.Created($"/{result.Data?.Id}", result)
+            : TypedResults.BadRequest(result.Data);
+    }
+}
