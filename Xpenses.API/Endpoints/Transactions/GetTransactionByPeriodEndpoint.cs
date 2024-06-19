42b93be8e9e7d05f76a8cd07454f5c90fa63a2e8
Vinicius <vini383@gmail.com>
2024-06-19 11:10:25 -0300

    add GetTransactionByPeriodEndpoint.cs

---
+++ Xpenses.API/Endpoints/Transactions/GetTransactionByPeriodEndpoint.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Xpenses.API.Common.Api;
+using Xpenses.Core;
+using Xpenses.Core.Entities;
+using Xpenses.Core.Handlers;
+using Xpenses.Core.Requests.Transactions;
+using Xpenses.Core.Responses;
+
+namespace Xpenses.API.Endpoints.Transactions;
+
+public class GetTransactionByPeriodEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+    =>
+        app.MapGet("/", HandleAsync)
+            .WithName("Transactions: GetByPeriod")
+            .WithSummary("Busca uma lista de transações pelo range de datas")
+            .Produces<PagedResponse<List<Transaction>?>>();
+
+    private static async Task<IResult> HandleAsync(ITransactionHandler handler, 
+        [FromQuery] DateTime? startDate = null,
+        [FromQuery] DateTime? endDate = null,
+        [FromQuery] int pageNumber = Configuration.DefaultPageNumber, 
+        [FromQuery] int pageSize = Configuration.DefaultPageSize)
+    {
+        var request = new GetTransactionsByPeriodRequest
+        {
+            UserId = "123",
+            PageNumber = pageNumber,
+            PageSize = pageSize,
+            StartDate = startDate,
+            EndDate = endDate
+        };
+
+        var result = await handler.GetByPeriodAsync(request);
+        return result.IsSuccess
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest(result.Data);
+    }
+
+}
