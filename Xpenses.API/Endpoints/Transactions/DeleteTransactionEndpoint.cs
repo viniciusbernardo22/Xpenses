5d6354e7e16b04e3fe9658b4f350fa644591e8e1
Vinicius <vini383@gmail.com>
2024-06-19 10:35:25 -0300

    add DeleteTransactionEndpoint.cs

---
+++ Xpenses.API/Endpoints/Transactions/DeleteTransactionEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using Xpenses.API.Common.Api;
+using Xpenses.Core.Entities;
+using Xpenses.Core.Handlers;
+using Xpenses.Core.Requests.Transactions;
+using Xpenses.Core.Responses;
+
+namespace Xpenses.API.Endpoints.Transactions;
+
+public class DeleteTransactionEndpoint : IEndpoint
+{
+    
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapPost("/", HandleAsync)
+            .WithName("Transactions: Delete")
+            .WithSummary("Exclui uma transação")
+            .Produces<Response<Transaction?>>();
+
+    private static async Task<IResult> HandleAsync(ITransactionHandler handler, DeleteTransactionRequest request)
+    {
+        request.UserId = "123";
+
+        var result = await handler.DeleteAsync(request);
+
+        return result.IsSuccess
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest(result.Data);
+    }
+}
