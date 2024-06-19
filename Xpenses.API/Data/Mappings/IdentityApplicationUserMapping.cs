ec91a42e8b9274b0dadb7327289faa07c20b138b
Vinicius <vini383@gmail.com>
2024-06-19 15:41:53 -0300

    feat: add IdentityApplicationUserMapping.cs

---
+++ Xpenses.API/Data/Mappings/IdentityApplicationUserMapping.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Xpenses.API.Models;
+
+namespace Xpenses.API.Data.Mappings;
+
+public class IdentityApplicationUserMapping : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> b)
+    {
+        b.ToTable("IdentityApplicationUser");
+        
+        b.HasKey( u => u.Id);
+
+        b.HasIndex(u => u.NormalizedUserName).IsUnique();
+        b.HasIndex(u => u.NormalizedEmail).IsUnique();
+
+        b.Property(u => u.Email).HasMaxLength(180);
+        b.Property(u => u.NormalizedEmail).HasMaxLength(180);
+        b.Property(u => u.UserName).HasMaxLength(180);
+        b.Property(u => u.NormalizedUserName).HasMaxLength(180);
+        b.Property(u => u.PhoneNumber).HasMaxLength(20);
+        b.Property(u => u.ConcurrencyStamp).IsConcurrencyToken();
+
+        b.HasMany<IdentityUserClaim<long>>().WithOne().HasForeignKey(u => u.UserId).IsRequired();
+        b.HasMany<IdentityUserLogin<long>>().WithOne().HasForeignKey(u => u.UserId).IsRequired();
+        b.HasMany<IdentityUserToken<long>>().WithOne().HasForeignKey(u => u.UserId).IsRequired();
+        b.HasMany<IdentityUserRole<long>>().WithOne().HasForeignKey(u => u.UserId).IsRequired();
+
+    }
+}
