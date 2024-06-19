b6847a23a054849498b9a3aafab076cd61f2eb8a
Vinicius <vini383@gmail.com>
2024-06-19 11:10:17 -0300

    add UpdateTransactionEndpoint.cs

---
+++ Xpenses.API/Endpoints/Transactions/UpdateTransactionEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Xpenses.API.Common.Api;
+using Xpenses.Core.Entities;
+using Xpenses.Core.Handlers;
+using Xpenses.Core.Requests.Transactions;
+using Xpenses.Core.Responses;
+
+namespace Xpenses.API.Endpoints.Transactions;
+
+public class UpdateTransactionEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapPut("/", HandleAsync)
+            .WithName("Transactions: Update")
+            .WithSummary("Atualiza uma transação")
+            .Produces<Response<Transaction?>>();
+
+    private static async Task<IResult> HandleAsync(ITransactionHandler handler, UpdateTransactionRequest request, long id)
+    {
+        request.UserId = "123";
+        request.Id = id;
+
+        var result = await handler.UpdateAsync(request);
+
+        return result.IsSuccess
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest(result.Data);
+    }
+
+}
