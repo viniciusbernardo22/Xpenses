25ec3bec41a1c0701d895dd6b5ca12711bc4bfff
Vinicius <vini383@gmail.com>
2024-06-19 16:00:11 -0300

    feat: add IdentityUserTokenMapping.cs

---
+++ Xpenses.API/Data/Mappings/Identity/IdentityUserTokenMapping.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Xpenses.API.Data.Mappings.Identity;
+
+public class IdentityUserTokenMapping : IEntityTypeConfiguration<IdentityUserToken<long>>
+{
+    public void Configure(EntityTypeBuilder<IdentityUserToken<long>> b)
+    {
+        b.ToTable("IdentityUserToken");
+        b.HasKey(t => new { t.UserId, t.LoginProvider, t.Name });
+        b.Property(t => t.LoginProvider).HasMaxLength(120);
+        b.Property(t => t.Name).HasMaxLength(180);
+    }
+}
