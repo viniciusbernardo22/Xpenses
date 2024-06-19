45200a4830a0a89b265e387d31417c3c56c6792a
Vinicius <vini383@gmail.com>
2024-06-19 15:23:07 -0300

    add ApplicationUser.cs

---
+++ Xpenses.API/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Xpenses.API.Models;
+
+public class ApplicationUser : IdentityUser<long>
+{
+    // RBAC 
+    public List<IdentityRole<long>>? Roles { get; set; }
+}
