4c49b567e273fcb92ec1f4cde7ca21d76d359820
Vinicius <vini383@gmail.com>
2024-06-19 11:10:55 -0300

    feat: mapping Transaction endpoints

--- Xpenses.API/Endpoints/Endpoint.cs
+++ Xpenses.API/Endpoints/Endpoint.cs
@@ -1,5 +1,6 @@
 ﻿using Xpenses.API.Common.Api;
 using Xpenses.API.Endpoints.Categories;
+using Xpenses.API.Endpoints.Transactions;
 
 namespace Xpenses.API.Endpoints;
 
@@ -7,14 +8,23 @@ public static class Endpoint
 {
     public static void MapEndpoints(this WebApplication app)
     {
-        var endpoints = app
-            .MapGroup("v1/categories")
-            .WithTags("Categories")
-            .MapEndpoint<CreateCategoryEndpoint>()
-            .MapEndpoint<GetAllCategoriesEndpoint>()
-            .MapEndpoint<GetCategoryByIdEndpoint>()
-            .MapEndpoint<UpdateCategoryEndpoint>()
-            .MapEndpoint<DeleteCategoryEndpoint>();
+        var endpoints = app;
+            
+            endpoints.MapGroup("v1/categories")
+                .WithTags("Categories")
+                    .MapEndpoint<CreateCategoryEndpoint>()
+                    .MapEndpoint<GetAllCategoriesEndpoint>()
+                    .MapEndpoint<GetCategoryByIdEndpoint>()
+                    .MapEndpoint<UpdateCategoryEndpoint>()
+                    .MapEndpoint<DeleteCategoryEndpoint>();
+
+            endpoints.MapGroup("v1/transactions")
+                .WithTags("Transactions")
+                .MapEndpoint<CreateTransactionEndpoint>()
+                .MapEndpoint<GetTransactionByIdEndpoint>()
+                .MapEndpoint<GetTransactionByPeriodEndpoint>()
+                .MapEndpoint<UpdateTransactionEndpoint>()
+                .MapEndpoint<DeleteTransactionEndpoint>(); 
     }
 
     private static IEndpointRouteBuilder MapEndpoint<T>(this IEndpointRouteBuilder app) where T : IEndpoint
