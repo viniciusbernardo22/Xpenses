817d25e18494c5edf137083076f7a01e71d88f1d
Vinicius <vini383@gmail.com>
2024-06-19 15:18:17 -0300

    config: dbcontext change to IdentityDbContext

--- Xpenses.API/Data/AppDbContext.cs
+++ Xpenses.API/Data/AppDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using System.Reflection;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Xpenses.Core.Entities;
 
 namespace Xpenses.API.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext(options)
 {
     public DbSet<Category> Categories { get; set; } = null!;
     public DbSet<Transaction> Transactions { get; set; } = null!;
