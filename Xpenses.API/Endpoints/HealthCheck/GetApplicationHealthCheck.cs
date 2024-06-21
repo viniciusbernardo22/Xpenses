3bcd7e8b77a27fd915580290b91cf073953bea54
Vinicius <vini383@gmail.com>
2024-06-21 10:45:42 -0300

    feat: padronize endpoint organization, added GetApplicationHealthCheck.cs

---
+++ Xpenses.API/Endpoints/HealthCheck/GetApplicationHealthCheck.cs
@@ -0,0 +1,11 @@
+﻿using Xpenses.API.Common.Api;
+
+namespace Xpenses.API.Endpoints.HealthCheck;
+
+public class GetApplicationHealthCheck : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+   => app.MapGet("/", () => "Ok")
+       .WithName("HealthCheck")
+       .WithSummary("Retorna status da aplicação");
+}
