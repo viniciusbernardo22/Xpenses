37280acd4ef5dc01d681b0cfd3a62f44e7637f21
Vinicius <vini383@gmail.com>
2024-06-21 14:40:44 -0300

    feat: add new migration

---
+++ Xpenses.API/Data/Migrations/20240621173830_v0.0.3.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Xpenses.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class v003 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "IdentityUser",
+                type: "nvarchar(180)",
+                maxLength: 180,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(180)",
+                oldMaxLength: 180,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "IdentityUser",
+                type: "nvarchar(180)",
+                maxLength: 180,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(180)",
+                oldMaxLength: 180);
+        }
+    }
+}
